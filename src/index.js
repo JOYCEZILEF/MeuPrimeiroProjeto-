d4527061b60604aa1ac6979acd6223166b16f92b
joycezilef <JOYCE_TEC@HOTMAIL.COM>
2024-03-17 13:29:22 -0300

    done

--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
 // O que acontece ao tentar reatribuir um valor a const idade? ou seja, criar uma segunda const idade = 30?
 const idade = 25;
 //console.log(idade); 
-
-
-
-
-
-////////////////////////////////////////////////////////////////////////
-
+//*******Resposta:Não podemos reatribuir um valor à variável que é declarada com const. Só podemos declarar uma vez.
 
 //O que acontece ao chamar a let x dentro e fora do escopo? 
 
+//*******Resposta:Let limita o escopo de uma variável declarada somente para aquela expressão
+
 //Escopo refere-se ao contexto no qual as variáveis e funções são definidas e acessadas.
+//*******Resposta:Sim
+
 //Nesse caso as chaves delimitam o nosso escopo
+//*******Resposta: Sim, as chaves delimitam o escopo.
+
 
 {
-    let x = 10;
+let x = 10;
    // console.log(x); 
 }
 // console.log(x);
-
-
-
-///////////////////////////////////////////////////////////////////////
 
 //Ainda sobre escopo explique a diferença dos console.log
 
@@ -34,7 +30,8 @@ const idade = 25;
 // console.log(x);
 // console.log(y);
 
-///////////////////////////////////////////////////////////////////////
+//*******Resposta:A única diferença entre as duas é o escopo em que essas variáveis existem, ariáveis declaradas com var têm escopo global ou de função, enquanto variáveis declaradas com let têm escopo de bloco.
+
 
 //O que acontece nessa declaração, explique sobre o Hoisting
 
@@ -43,30 +40,24 @@ var a = 5;
 console.log(b);
 //let b = 10;
 
+//*******Resposta:O Hoisting é o comportamento padrão de mover as declarações para o topo do seu escopo antes da execução do código, não importa se o escopo seja global ou local.
 
-/////////////////////////////////////////////////////////////////////////////////
 
 //No código abaixo apresenta um erro, qual?
 
-var x = 5;
-var x = 10;
-// console.log(x); 
+// var x = 5;
+// var x = 10;
+//  console.log(x); 
 
-let y = 15;
-let y = 20; 
+// let y = 15;
+// let y = 20; 
 // console.log(y); 
 
+// *******Resposta: Há erro de sintaxe, pois a variável y já foi declarada.
 
-//// Escreva um programa que print a primeira letra dessa variavel 
-let nomeCompleto = 'eduardo'
+///Escreva um programa que print a primeira letra dessa variavel 
+let nomeCompleto = 'Joyce'
 
 //sua lógica para pegar a primeira letra 
-/// const letraInicial = ...
-//console.log(letraInicial)
-
-
-
-
-
-
-
+const letraInicial = nomeCompleto[0];
+console.log(letraInicial)
